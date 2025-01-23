79c24563003808b1672034967879828e393c5c7c
KannanSudha <kannansudha013@gmail.com>
2025-01-23 08:33:53 +0530

    code is added

---
+++ reference.java
@@ -0,0 +1,30 @@
+package Sudha;
+
+import java.util.List;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class reference {
+	
+	public static void main(String[] args) {
+		
+		WebDriver driver = new ChromeDriver();
+		driver.get("https://www.flipkart.com");
+		driver.manage().window().maximize();
+		WebElement mbl = driver.findElement(By.xpath("//input[@type='text']"));
+		mbl.sendKeys("Realme Mobiles",Keys.ENTER);
+		
+		List<WebElement> realme = driver.findElements(By.xpath("//*[contains(text(),'realme')]"));
+		System.out.println("No.of Count in Realme Mobiles "+realme.size());
+		for (int i = 0; i < realme.size(); i++) {
+			WebElement size = realme.get(i);
+			System.out.println(size.getText());
+		}
+			
+	}
+
+}
